a2a46c6f28e6f08882315d8543841d78308ab844
Robel-robel <robelberhanu89@gmail.com>
2022-04-22 21:42:53 +0200

    updated main.java

--- MarsRoverApi/src/main/java/com/coderscampus/MarsRoverApiApplication.java
+++ MarsRoverApi/src/main/java/com/coderscampus/MarsRoverApiApplication.java
@@ -11,18 +11,3 @@ public class MarsRoverApiApplication {
 	}
 
 }
-
-package com.coderscampus;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MarsRoverApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MarsRoverApiApplication.class, args);
-	}
-
-}
-
