ecf6e6c6f7697c35a467da5d79e3a729d53ffc69
Robel-robel <robelberhanu89@gmail.com>
2022-05-08 23:06:08 +0200

    modified mvc

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     
     @GetMapping("/")
-    public String getHome(){
+    public String getHome(Model model){
+        model.addAttribute("name", "Robel");
         return "index";
     }
 }
