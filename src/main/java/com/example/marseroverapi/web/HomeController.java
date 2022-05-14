d2028bbca41cc2873af599e966202a57c1e38bf6
Robel-robel <robelberhanu89@gmail.com>
2022-05-14 21:47:56 +0200

    display images on index.html

--- src/main/java/com/example/marseroverapi/web/HomeController.java
+++ src/main/java/com/example/marseroverapi/web/HomeController.java
@@ -6,6 +6,7 @@ import com.example.marseroverapi.service.MarsRoverApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,7 +16,7 @@ public class HomeController {
     private MarsRoverApiService roverService;
     
     @GetMapping("/")
-    public String getHome(Model model){
+    public String getHome(ModelMap model){
         MarsRoverApiResponse roverData = roverService.getRoverData();
         model.put("roverData", roverData);
         return "index";
