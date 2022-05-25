133e3187cd6c1651da7f6f0e8caff7bd908d5048
Robel-robel <robelberhanu89@gmail.com>
2022-05-25 14:59:43 +0200

    added a functionality to pull mars images for different days/sol

--- target/classes/static/js/index.js
+++ target/classes/static/js/index.js
@@ -11,6 +11,8 @@ marsApiButtons.forEach(button => button.addEventListener('click', function (){
 
                                 }))
 
+
+// function to get Url parameter, ie rover type in this case
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
@@ -18,4 +20,19 @@ function getUrlParameter(name) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-getUrlParameter("marsApiRoverData");
+let marsRoverType = getUrlParameter("marsApiRoverData");
+
+highlightBtnByRoverType(marsRoverType)
+
+//update sol form button to match variable for request parameter
+let marsSol = getUrlParameter('marsSol')
+document.getElementById('marsSol').value = marsSol
+
+// utility function to highlight buttons
+function highlightBtnByRoverType(roverType){
+    if (roverType == '')
+        roverType = 'Opportunity'
+    
+    document.getElementById('marsApi'+roverType).classList.remove('btn-secondary')
+    document.getElementById('marsApi'+roverType).classList.add('btn-primary')
+}
