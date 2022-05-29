1243e025f6f3e9c80240b4df40a0ae9395c962b6
Robel-robel <robelberhanu89@gmail.com>
2022-05-29 18:27:52 +0200

    added getters and setters for HomeDto.java

--- src/main/java/com/example/marseroverapi/dto/HomeDto.java
+++ src/main/java/com/example/marseroverapi/dto/HomeDto.java
@@ -6,8 +6,29 @@ public class HomeDto {
     private Integer marsSol;
     private Boolean defaultCheck1;
 
+
     // Getters and Setters
-  
+    public String getMarsApiRoverData() {
+        return marsApiRoverData;
+    }
+    public void setMarsApiRoverData(String marsApiRoverData) {
+        this.marsApiRoverData = marsApiRoverData;
+    }
+    public Integer getMarsSol() {
+        return marsSol;
+    }
+    public void setMarsSol(Integer marsSol) {
+        this.marsSol = marsSol;
+    }
+    public Boolean getDefaultCheck1() {
+        return defaultCheck1;
+    }
+    public void setDefaultCheck1(Boolean defaultCheck1) {
+        this.defaultCheck1 = defaultCheck1;
+    }
+
+    
+    
 
     
     
--- src/main/java/com/example/marseroverapi/service/MarsRoverApiService.java
+++ src/main/java/com/example/marseroverapi/service/MarsRoverApiService.java
@@ -10,11 +10,11 @@ import org.springframework.web.client.RestTemplate;
 public class MarsRoverApiService {
     
     private static final String API_KEY = "0fI6hFwXLPbSRfCNK76NtCJaeBNGkgH4urhvsMlu";
-    public MarsRoverApiResponse getRoverData(String roverType, Integer marsSol){
+
+    public MarsRoverApiResponse getRoverData(String roverType, Integer marsSol){ 
         
         RestTemplate rt = new RestTemplate(); // a way to issue requests(GET, POST, PUT, DELETE..), take data and map it to our own objects. 
 		ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverType+"/photos?sol="+marsSol+"&api_key=" + API_KEY, MarsRoverApiResponse.class );
-        
         return response.getBody();
     }
 }
